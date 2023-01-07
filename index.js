1cab473440096364dabdcf7ab853b6ea92c4f16e
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-01-07 21:14:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let startScore = 0;
+let liveScoreHome = 0;
+let liveScoreGuest = 0;
+
+renderNewGame();
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.oneHome) {
+    console.log("oneHome:", e.target.dataset.oneHome);
+    liveScoreHome++;
+    renderHomeScore();
+  } else if (e.target.dataset.twoHome) {
+    console.log("twoHome:", e.target.dataset.twoHome);
+    liveScoreHome += 2;
+    renderHomeScore();
+  } else if (e.target.dataset.threeHome) {
+    console.log("threeHome:", e.target.dataset.threeHome);
+    liveScoreHome += 3;
+    renderHomeScore();
+  } else if (e.target.dataset.oneGuest) {
+    console.log("oneGuest:", e.target.dataset.oneGuest);
+    liveScoreGuest++;
+    renderGuestScore();
+  } else if (e.target.dataset.twoGuest) {
+    liveScoreGuest += 2;
+    console.log("twoGuest:", e.target.dataset.twoGuest);
+    renderGuestScore();
+  } else if (e.target.dataset.threeGuest) {
+    liveScoreGuest += 3;
+    console.log("threeGuest:", e.target.dataset.threeGuest);
+    renderGuestScore();
+  }
+});
+
+document.getElementById("newGame").addEventListener("click", function () {
+  renderNewGame();
+});
+function renderNewGame() {
+  startScore = 0;
+  liveScoreHome = 0;
+  liveScoreGuest = 0;
+  document.getElementById("homeScore").innerHTML = `<p>${startScore}</p>`;
+  document.getElementById("guestScore").innerHTML = `<p>${startScore}</p>`;
+}
+function renderHomeScore() {
+  document.getElementById("homeScore").innerHTML = `<p>${liveScoreHome}</p>`;
+}
+
+function renderGuestScore() {
+  document.getElementById("guestScore").innerHTML = `<p>${liveScoreGuest}</p>`;
+}
